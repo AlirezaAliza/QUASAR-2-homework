27babfbe069977b9dde57bebb9ae539aaed3990a
alireza <alireza.bmw.1295@gmail.com>
2024-11-24 15:29:15 +0330

    HomeWork 5

--- src/pages/Dashboard/IndexPage.vue
+++ src/pages/Dashboard/IndexPage.vue
@@ -1,17 +1,26 @@
-<!-- <template>
+<template>
+  <q-page class="q-pa-md row justify-center items-start q-gutter-md">
     <q-card
-      class="my-card text-white"
+      v-for="post in posts"
+      :key="post"
+      class="card-sp text-white"
       style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
     >
       <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
+        <div class="text-h6">{{ post.title}}</div>
+        <div class="text-subtitle2">{{ post.text }}</div>
       </q-card-section>
 
       <q-card-section class="q-pt-none">
-        {{ lorem }}
+        {{ post.author }}
       </q-card-section>
     </q-card>
 
+  </q-page>
 </template>
- -->
+
+<script setup>
+import { posts } from 'src/components/ts/IndexPost';
+</script>
+
+<!--  -->
